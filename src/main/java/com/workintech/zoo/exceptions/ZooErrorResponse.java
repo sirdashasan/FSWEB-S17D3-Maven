a0287d946bcb31b6a931d67e504a935b772b8650
sirdashasan <sirdashasan@hotmail.com>
2024-06-27 14:55:34 +0300

    Tüm görevler tamamlandı

---
+++ src/main/java/com/workintech/zoo/exceptions/ZooErrorResponse.java
@@ -0,0 +1,15 @@
+package com.workintech.zoo.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ZooErrorResponse {
+    private Integer status;
+    private String message;
+    private Long timestamp;
+
+}
